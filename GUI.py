cf70c29bad014e908b6303f980be1a18795a3809
Arya-A-Nair <aryaajitnair@gmail.com>
2021-08-03 23:51:42 +0530

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -2,31 +2,62 @@ from tkinter import *
 from selenium import webdriver
 from time import sleep 
 
-def login():
-    username=receiver.get("1.0","end-1c")
-    msg=message.get("1.0","end-1c")
-    driver = webdriver.Chrome()
-    url='https://www.instagram.com/accounts/login/'
-    driver.get(url)
-    sleep(1)
-    driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input').send_keys('python_test123456789')
-    driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input').send_keys('pythongod')
-    driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button').click()
-    sleep(5)
-    
-    driver.get('https://www.instagram.com/direct/inbox/')
-    driver.find_element_by_xpath('/html/body/div[3]/div/div/div/div[3]/button[2]').click()
-    driver.find_element_by_xpath('//*[@id="react-root"]/section/div/div[2]/div/div/div[1]/div[1]/div/div[3]/button').click()
-    driver.find_element_by_xpath('/html/body/div[5]/div/div/div[2]/div[1]/div/div[2]/input').send_keys(username)
-    sleep(2)
-    driver.find_element_by_xpath('/html/body/div[5]/div/div/div[2]/div[2]/div[1]/div/div[3]/button').click()
-    sleep(2)
-    driver.find_element_by_xpath('/html/body/div[5]/div/div/div[1]/div/div[2]/div/button').click()
-    sleep(2)
-    driver.find_element_by_xpath('//*[@id="react-root"]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[2]/textarea').send_keys(msg)
-    sleep(1)
-    driver.find_element_by_xpath('//*[@id="react-root"]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[2]/textarea').send_keys(u'\ue007')
 
+def new_page():
+    def login():
+        
+        user=usern.get("1.0","end-1c")
+        pswd=password.get("1.0","end-1c")
+        username=receiver.get("1.0","end-1c")
+        msg=message.get("1.0","end-1c")
+        driver = webdriver.Chrome()
+        url='https://www.instagram.com/accounts/login/'
+        driver.get(url)
+        sleep(1)
+        driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input').send_keys(user)
+        driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input').send_keys(pswd)
+        driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button').click()
+        sleep(5)
+        
+        driver.get('https://www.instagram.com/direct/inbox/')
+        driver.find_element_by_xpath('/html/body/div[3]/div/div/div/div[3]/button[2]').click()
+        driver.find_element_by_xpath('//*[@id="react-root"]/section/div/div[2]/div/div/div[1]/div[1]/div/div[3]/button').click()
+        driver.find_element_by_xpath('/html/body/div[5]/div/div/div[2]/div[1]/div/div[2]/input').send_keys(username)
+        sleep(2)
+        driver.find_element_by_xpath('/html/body/div[5]/div/div/div[2]/div[2]/div[1]/div/div[3]/button').click()
+        sleep(2)
+        driver.find_element_by_xpath('/html/body/div[5]/div/div/div[1]/div/div[2]/div/button').click()
+        sleep(2)
+        driver.find_element_by_xpath('//*[@id="react-root"]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[2]/textarea').send_keys(msg)
+        sleep(1)
+        driver.find_element_by_xpath('//*[@id="react-root"]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[2]/textarea').send_keys(u'\ue007')
+        driver.close()
+        window.quit()
+    
+
+    global lbl,lbl2,button,usern,password
+    lbl.grid_forget()
+    lbl2.grid_forget()
+
+
+
+    
+    lbl=Label(right_frame,text="Who would you like to send it to",bg="skyblue")
+    lbl.grid(row=0, column=1)
+
+    receiver=Text(right_frame, height=1,width=15)
+    receiver.grid(row=0, column=2)
+
+
+    lbl2=Label(right_frame,text="What is the message you want to send",bg="skyblue")
+    lbl2.grid(row=1, column=1)
+
+    message=Text(right_frame,height=1,width=15)
+    message.grid(row=1, column=2)
+
+    
+    button = Button(right_frame,text="Submit",command=login)
+    button.grid(row=2, column=1)
 
 
 window=Tk()
@@ -46,23 +77,23 @@ original_image = image1.subsample(3,3)
 Label(left_frame, image=original_image).grid(row=1, column=0, padx=5, pady=5)
 
 
-
-lbl=Label(right_frame,text="Who would you like to send it to",bg="skyblue")
+lbl=Label(right_frame,text="Enter your username",bg="skyblue")
 lbl.grid(row=0, column=1)
 
-receiver=Text(right_frame, height=1,width=15)
-receiver.grid(row=0, column=2)
+usern=Text(right_frame, height=1,width=15)
+usern.grid(row=0, column=2)
 
 
-lbl2=Label(right_frame,text="what is the message you want to send",bg="skyblue")
+lbl2=Label(right_frame,text="Enter your password",bg="skyblue")
 lbl2.grid(row=1, column=1)
 
-message=Text(right_frame,height=1,width=15)
-message.grid(row=1, column=2)
+password=Text(right_frame,height=1,width=15)
+password.grid(row=1, column=2)
 
 
-button = Button(right_frame,text="Click me!",command=login)
+button = Button(right_frame,text="Next->",command=new_page)
 button.grid(row=2, column=1)
 
 
-window.mainloop()
+mainloop()
+
